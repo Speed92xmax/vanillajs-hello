6d90a16cd6072534ac22fe8bf5fcb185423cfc60
Kevin Pacheco <91635416+Speed92xmax@users.noreply.github.com>
2023-10-11 22:40:53 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,29 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let subject = ["Yo ", "Tu ", "El ", "Ella ", "Nosotros "];
+  let verb = [
+    "estuvimos en ",
+    "fuimos a ",
+    "tuvimos que ",
+    "saltamos a ",
+    "nos corrieron "
+  ];
+  let rest = [
+    "sin zapatos en la playa.",
+    "dentro de su casa.",
+    "debajo de un coche.",
+    "una montaña.",
+    "sin saber."
+  ];
+
+  let subNum = Math.floor(Math.random() * 5);
+  let verbNum = Math.floor(Math.random() * 5);
+  let restNum = Math.floor(Math.random() * 5);
+
+  let $excussesContainer = (document.querySelector("#container").innerHTML =
+    subject[subNum] + verb[verbNum] + rest[restNum]);
+
+  console.log($excussesContainer);
 };
